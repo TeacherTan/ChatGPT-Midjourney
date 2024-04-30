34e22ae7c62c361226c776b5e32a73e20b262c45
Neal_Tan <425088158@qq.com>
2024-04-30 10:43:18 +0800

    style(config): 修改默认值

--- app/store/config.ts
+++ app/store/config.ts
@@ -40,7 +40,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "stable-diffusion" as ModelType,
+    model: "llama 3" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 2000,
